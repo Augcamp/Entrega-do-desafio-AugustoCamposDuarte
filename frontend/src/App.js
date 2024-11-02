4fdebae3b0af2c1466ebda3b098aa11b437771d9
Augcamp <87672417+Augcamp@users.noreply.github.com>
2024-11-02 20:31:58 -0300

    add ordenacao A-Z

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { addUser, getUsers, deleteUser, toggleStar } from './api'; 
+import { addUser, getUsers, deleteUser, toggleStar } from './api';
 import UserList from './components/UserList';
-
+import './App.css';
 
 function App() {
     const [username, setUsername] = useState('');
     const [users, setUsers] = useState([]);
-    
+    const [isSorted, setIsSorted] = useState(false); // Estado para controlar a ordenação
+
     useEffect(() => {
         const fetchUsers = async () => {
             try {
@@ -44,8 +45,8 @@ function App() {
 
     const handleToggleFavorite = async (username) => {
         try {
-            await toggleStar(username); 
-            setUsers(users.map(user => 
+            await toggleStar(username);
+            setUsers(users.map(user =>
                 user.username === username ? { ...user, favorite: true } : { ...user, favorite: false }
             ));
         } catch (error) {
@@ -53,10 +54,37 @@ function App() {
         }
     };
 
+    
+    const sortUsersAlphabetically = () => {
+        const sortedUsers = [...users].sort((a, b) => 
+            a.username.localeCompare(b.username)
+        );
+        setUsers(sortedUsers);
+        setIsSorted(true);
+    };
+
+    const unsortUsers = async () => {
+        try {
+            const response = await getUsers();
+            setUsers(response.data);
+        } catch (error) {
+            console.error("Erro ao restaurar a lista original:", error);
+        }
+        setIsSorted(false);
+    };
+
+    const toggleSort = () => {
+        if (isSorted) {
+            unsortUsers();
+        } else {
+            sortUsersAlphabetically();
+        }
+    };
+
     return (
         <div className="app">
           <h1>Favoritos do GitHub</h1>
-    
+
           <div className="input-container">
             <input
               type="text"
@@ -71,7 +99,11 @@ function App() {
             />
             <button onClick={handleAddUser}>Adicionar</button>
           </div>
-    
+
+          <button onClick={toggleSort}>
+              {isSorted ? "Remover Ordenação" : "Ordenar A-Z"}
+          </button>
+
           <UserList
             users={users}
             onDeleteUser={handleDeleteUser}
